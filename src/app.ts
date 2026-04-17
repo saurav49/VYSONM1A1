26b34d6a9f5da740c767b887c2847196eedc8905
sauravbYMIR <sauravbiswas@ymirlabs.com>
2026-04-18 02:21:08 +0530

    feat: batch insert api added

--- src/app.ts
+++ src/app.ts
@@ -36,6 +36,15 @@ routes.get('/ping', (_req, res) => {
     message: 'Server up and running',
   });
 });
+// routes.get('/check-db-size', async (_req, res) => {
+//   const r = await query(
+//     `SELECT pg_size_pretty(pg_total_relation_size('url_shortener'));`,
+//   );
+//   return res.status(200).json({
+//     status: true,
+//     data: r?.rows[0],
+//   });
+// });
 
 routes.post('/short', async (req, res) => {
   try {
@@ -68,6 +77,54 @@ routes.post('/short', async (req, res) => {
       data: {
         originalUrl,
       },
+    });
+  } catch (e) {
+    return res.status(500).json({
+      status: false,
+      error: e,
+    });
+  }
+});
+routes.post('/batch-insert-short', async (req, res) => {
+  try {
+    const start = performance.now();
+    const { originalUrl, iteration } = req.body;
+    if (typeof iteration !== 'number' || iteration <= 0) {
+      return res.status(400).json({
+        status: false,
+        message: 'Invalid iteration',
+      });
+    }
+    if (!originalUrl) {
+      return res.status(400).json({
+        status: false,
+        message: 'Original url is required',
+      });
+    }
+    for (let i = 0; i < iteration; i++) {
+      let shortCode = '';
+      shortCode = nanoid(7);
+      const q = await query(
+        `
+                SELECT * 
+                FROM "url_shortener"
+                WHERE short_code=$1
+                `,
+        [shortCode],
+      );
+      while (q?.rowCount && q.rowCount > 0) {
+        shortCode = nanoid(7);
+      }
+      await insertIntoTable({
+        original_url: originalUrl,
+        short_code: shortCode,
+      });
+    }
+    const end = performance.now();
+    console.log(`API took ${(end - start).toFixed(2)} ms`);
+    return res.status(201).json({
+      status: true,
+      message: 'Successful!!',
     });
   } catch (e) {
     return res.status(500).json({
