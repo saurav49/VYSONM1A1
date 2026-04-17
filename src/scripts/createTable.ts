62bf52e9376e1428d5b2543fd7c8e0f61812c774
sauravbYMIR <sauravbiswas@ymirlabs.com>
2026-04-17 23:38:17 +0530

    feat: setup db, added createtabvle script

---
+++ src/scripts/createTable.ts
@@ -0,0 +1,19 @@
+import { query } from '../config/db';
+
+async function createTable() {
+  try {
+    await query(`
+              CREATE TABLE url_shortener 
+              (
+                  id SERIAL PRIMARY KEY,
+                  original_url TEXT NOT NULL,
+                  short TEXT NOT NULL UNIQUE,
+                  created_at TIMESTAMPTZ DEFAULT NOW()
+              )
+              `);
+    console.log(`url_shortener created succesfully`);
+  } catch (e) {
+    console.error(e);
+  }
+}
+createTable();
