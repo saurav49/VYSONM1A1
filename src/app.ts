4c2ec597152d4273b47e2864b375c5d33473f2c6
sauravbYMIR <sauravbiswas@ymirlabs.com>
2026-04-20 02:12:33 +0530

    feat: create user endpoint created, email/name validation added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Router } from 'express';
 import cors from 'cors';
+import validator from 'validator';
+import { query } from './db/client';
 
 const app = express();
 const routes = Router();
@@ -18,6 +20,42 @@ routes.get('/health', (_req, res) => {
     status: true,
     message: 'Server up and running',
   });
+});
+
+routes.post('/todos', async (req, res) => {});
+
+routes.post('/users', async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const isValidEmail = validator.isEmail(email);
+    if (!name) {
+      return res.status(401).json({
+        status: false,
+        message: 'Name is a required field',
+      });
+    }
+    if (!isValidEmail) {
+      return res.status(401).json({
+        status: false,
+        message: 'Invalid email',
+      });
+    }
+    const result = await query(
+      `
+        INSERT INTO users (name, email)
+        VALUES ($1, $2)
+        RETURNING *
+      `,
+      [name, email],
+    );
+    return res.status(201).json({
+      status: true,
+      data: result?.rows,
+    });
+  } catch (e) {
+    console.log({ e });
+    throw e;
+  }
 });
 
 export default app;
