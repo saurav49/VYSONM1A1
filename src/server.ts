eb7d824f6a609c54e01b72dbfe10ce969974a721
sauravbYMIR <sauravbiswas@ymirlabs.com>
2026-04-19 15:20:53 +0530

    feat: setup bun

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-import { config } from './config/env';
-
-const PORT = config.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
