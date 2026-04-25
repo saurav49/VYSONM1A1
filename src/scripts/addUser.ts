a3b9eccd1f5d8a706097925f03947bfb65a6bba0
sauravbYMIR <sauravbiswas@ymirlabs.com>
2026-04-26 00:45:07 +0530

    feat: add user script wip 2

--- src/scripts/addUser.ts
+++ src/scripts/addUser.ts
@@ -1,36 +1,62 @@
 import { query } from '../db/client';
 import { faker } from '@faker-js/faker';
 
-const INTERATIONS = 1000000;
-const BATCH_SIZE = 3000;
+const INTERATIONS = 1;
+const BATCH_SIZE = 1;
 
 async function addUser() {
+  const start = performance.now();
   for (let i = 0; i < INTERATIONS; i += BATCH_SIZE) {
     try {
-      const start = performance.now();
       await query('BEGIN');
       let values = [];
       for (let j = 0; j < BATCH_SIZE; j++) {
-        const name = faker.internet.username();
-        const email = faker.internet.email();
-        values.push(`(${name},${email})`);
+        values.push(
+          `(${faker.internet.displayName()}, ${faker.internet.email()})`,
+        );
       }
-      await query(
+      console.log(`
+                  INSERT INTO users (name, email)
+                  VALUES ${values.join(',')}
+                  ON CONFLICT (id) DO NOTHING
+                  RETURNING *
+              `);
+      const result = await query(
         `
-                INSERT INTO user (name, email)
-                VALUES ()
-            `,
-        [values.join(',')],
+                  INSERT INTO users (name, email)
+                  VALUES ${values.join(',')}
+                  ON CONFLICT (id) DO NOTHING
+                  RETURNING *
+              `,
       );
+      let failedCount = result?.rowCount
+        ? values.length - result.rowCount
+        : values.length;
+      let failedInserts = [];
+      while (failedCount > 0) {
+        for (let k = 0; k < failedCount; k++) {
+          failedInserts.push(
+            `(${faker.internet.displayName()}, ${faker.internet.email()})`,
+          );
+        }
+        const result = await query(`
+                INSERT INTO users (name, email)
+                VALUES ${failedInserts.join(',')}
+                RETURNING *
+            `);
+        failedCount = result?.rowCount
+          ? values.length - result.rowCount
+          : values.length;
+      }
       await query('COMMIT');
-      const end = performance.now();
-      console.log(
-        `API (/batch-insert-1million-user) took ${(end - start).toFixed(2)} ms`,
-      );
     } catch (e) {
       console.error({ e });
       await query('ROLLBACK');
     }
   }
+  const end = performance.now();
+  console.log(
+    `API (/batch-insert-1million-user) took ${(end - start).toFixed(2)} ms`,
+  );
 }
 addUser();
